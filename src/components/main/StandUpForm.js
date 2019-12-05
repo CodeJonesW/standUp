ee063d2aab36bb0b7ff89c4784f63194cbc47ce5
Will Jones <willjones2010@gmail.com>
2019-12-05 01:40:51 -0500

    created form for standUps

--- src/components/main/StandUpForm.js
+++ src/components/main/StandUpForm.js
@@ -7,8 +7,28 @@ class StandUpForm extends Component {
     }
     render() {
         return (
-            <div id="standUpForm">
-
+            <div id="standUpFormDiv">
+                <form id="standUpForm" action="/">
+                    <div class="item">
+                        <label>Yesterday:</label>
+                        <textarea rows="1"></textarea>
+                    </div>
+                    <div class="item">
+                        <label>Today:</label>
+                        <textarea rows="1"></textarea>
+                    </div>
+                    <div class="item">
+                        <label>Blockers:</label>
+                        <textarea rows="1"></textarea>
+                    </div>
+                    <div class="item">
+                        <p>Additional comment(s):</p>
+                        <textarea rows="1"></textarea>
+                    </div>
+                    <div class="btn-block">
+                        <button type="submit" href="/">Send Complaint to HR</button>
+                    </div>
+                </form>
             </div>
         );
     }
