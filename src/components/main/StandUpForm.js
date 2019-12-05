40744392d8bf2d75fda3aba45fbc8be5c5d6a2aa
Will Jones <willjones2010@gmail.com>
2019-12-05 13:35:44 -0500

    css

--- src/components/main/StandUpForm.js
+++ src/components/main/StandUpForm.js
@@ -10,19 +10,17 @@ class StandUpForm extends Component {
             <div id="standUpFormDiv">
                 <form id="standUpForm" action="/">
                     <div class="item">
-
                         <textarea placeholder="Yesterday:" rows="2"></textarea>
                     </div>
                     <div class="item">
-
                         <textarea placeholder="Today:" rows="2"></textarea>
                     </div>
                     <div class="item">
-
                         <textarea placeholder="Blockers:" rows="2"></textarea>
                     </div>
                     <div class="item">
-                        Seeking help? <input name="help" type="checkbox" />
+                        Seeking help?<input name="help" type="checkbox" />
+                        PR needs Review<input name="PR" type="checkbox" />
                     </div>
                     <div class="btn-block">
                         <button type="submit" href="/">standUp</button>
