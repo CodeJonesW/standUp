2db25feff68dec7b790fe301d56701d35747b572
Will Jones <willjones2010@gmail.com>
2019-12-24 03:45:50 -0500

    update

--- src/components/main/Display.js
+++ src/components/main/Display.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
 import StandUp from './StandUp'
 import StandUpForm from './StandUpForm'
+
+// const PROXY = "https://cors-anywhere.herokuapp.com/"
+
 class Display extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            standUps: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:3000/standups')
+            .then(res => res.json())
+            .then(data => this.setState({ standUps: data }))
     }
 
     render() {
         return (
             <div id="display">
 
-                <StandUp />
+                <StandUp standups={this.state.standUps} />
                 <StandUpForm />
+
+
             </div>
         );
     }
