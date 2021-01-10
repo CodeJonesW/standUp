b68e87d35f7770361d4843afa20a2506c576979e
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2021-01-09 21:53:44 -0500

    bad div

--- src/components/main/StandUp.js
+++ src/components/main/StandUp.js
@@ -2,15 +2,12 @@ import React from 'react';
 
 function StandUp(props) {
     return (
-        <div>
-            <ul id="standUp">
-                <li> Yesterday: {props.yesterday}</li>
-                <li> Today: {props.today}</li>
-                <li> Blocker: {props.blocker}</li>
-                <button onClick={props.deleteStandUp} className="myDeleteButton"> <span aria-labelledby="jsx-a11y/aria-proptypes" role="img" alt="emoji ">❌</span> </button>
-            </ul>
-        </div>
-
+        <ul id="standUp">
+            <li> Yesterday: {props.yesterday}</li>
+            <li> Today: {props.today}</li>
+            <li> Blocker: {props.blocker}</li>
+            <button onClick={props.deleteStandUp} className="myDeleteButton"> <span aria-labelledby="jsx-a11y/aria-proptypes" role="img" alt="emoji ">❌</span> </button>
+        </ul>
     );
   }
 export default StandUp;
